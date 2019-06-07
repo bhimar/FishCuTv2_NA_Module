5b63888d94223220b7ae2bf5c48606bfaf62935e
Rehaan <bhimar@uw.edu>
2019-06-07 10:41:17 -0700

    collected data from module

--- NeurSpaceAShapeClient.m
+++ NeurSpaceAShapeClient.m
@@ -1,24 +1,28 @@
 %Client for NeurSpaceAShape.m function
 
 %load previous segmentation
-filePath = ['Z:\Rehaan Bhimani\FishCuTv2 Project\' ...
-            'FishCuTv2 Modules\Test Data\bmp1a germline mutants\' ...
-            'germ4\02-Jun-2016\'];
+filePath = ['Z:\Rehaan Bhimani\FishCuTv2 Project\FishCuTv2 Modules\Test Data\bmp1a crispants\msbl 95.AB bmp1a 07 Analysis Files\'];
 load([filePath 'ReconstructedSegmentation']);
 NaMajAx = [];
 NaMinAx = [];
 NaArea = [];
 NaEffArea = [];
+Rows = {};
 for n = 1:21
+    Rows{n} = ['Vertebrae' num2str(n)];
     neuralImage = NeuralArchImages{n};
     [NaMajAx,NaMinAx,NaArea,NaEffArea] = NeurSpaceAShape(n,neuralImage,NaMajAx,NaMinAx,NaArea,NaEffArea);
 end
 
-%write to file
+%write to file for r^2 comparison evaluation
 
 % filePath2 = 'Z:\Rehaan Bhimani\FishCuTv2 Project\FishCuTv2 Modules\Test Data\';
 % fishSample = 'bmp1aGermlineNAs.xlsx';
 % table = xlsread([filePath2 fishSample]);
 % table(:,size(table,2)+1) = NaMajAx;
-% xlswrite([filePath2 fishSample], table);
+% xlswrite([filePath2 fishSample], Stable);
+
+%write to file for global test evaluation
+%T = table(Rows', NaMajAx', NaMinAx', NaArea', NaEffArea','VariableNames',{'Row','NaMajAx','NaMinAx','NaArea','NaEffArea'});
+%writetable(T, [filePath 'NaData.txt'], 'Delimiter', '\t');
 
